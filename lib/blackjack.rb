bad61878b4b94e6b7203186d3f6fe45e6fcdaf42
Jack Staffen <jack.staffen@gmail.com>
2019-10-05 02:23:37 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,77 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  sum = 0
+  2.times {| | sum += deal_card }
+  display_card_total(sum)
+  sum
 end
 
 def invalid_command
-  # code invalid_command here
+  puts 'Please enter a valid command'
 end
+
+def hit?(total)
+  prompt_user
+  user_input = get_user_input
+  while user_input !='s' && user_input !='h'
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+  if user_input == 's'
+    total
+  elsif user_input == 'h'
+    total += deal_card
+  end
+end
+
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  # binding.pry
+  total = initial_round
+  until total > 21 do
+    total = hit?(total)
+    display_card_total(total)
+  end
+  end_game(total)
 end
-    
+  # if user_input == 's'
+  #   display_card_total(total)
+  # else
+  #   until total > 21 do
+  #     total += hit?(total)
+  #     display_card_total(total)
+  #   end
+  # end
+  # end_game(total)
